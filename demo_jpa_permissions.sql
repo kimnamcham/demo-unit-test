030388a1842b7919002e8d63b3096d732d2530c6
minhdv <minhdv@gogroup.com.vn>
2023-03-31 13:03:38 +0700

    //

---
+++ demo_jpa_permissions.sql
@@ -0,0 +1,5 @@
+insert into demo_jpa.permissions (id, name)
+values  ('CREATE_ROLE', 'Thêm vai trò quyền người dùng'),
+        ('CREATE_USER', 'Thêm người dùng'),
+        ('UPDATE_ROLE', 'Cập nhật vai trò người dùng'),
+        ('UPDATE_USER', 'Cập nhật người dùng');
